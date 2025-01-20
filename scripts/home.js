9d875eb9ff663ee729abf6c69d9b09e4683f3529
Ege Uysal <ahmetegeuysal@gmail.com>
2025-01-20 15:31:12 -0600

    finish building basic home.html

---
+++ scripts/home.js
@@ -0,0 +1,28 @@
+function createAccount() {
+    window.location.href = "/login/create-account.html";
+}
+
+const createAccountButton = document.querySelector('.create-account');
+if (createAccountButton) {
+    createAccountButton.addEventListener('click', createAccount);
+}
+
+function signIn() {
+    window.location.href = "/login/sign-in.html";
+}
+
+const signInButton = document.querySelector('.sign-in');
+if (signInButton) {
+    signInButton.addEventListener('click', signIn);
+}
+
+function goBack () {
+    window.location.href = "/index.html";
+}
+
+const goBackButton = document.querySelector('.recent-page')
+if (goBackButton) {
+    goBackButton.addEventListener('click', goBack)
+}
+
+window.addEventListener('load', function() {
